c243a26097e1d8929d041dbdf48465609f19f21f
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-11 18:57:41 +0100

    Adds fun facts for all animals

--- src/main/java/se/frusunnanbo/servicec/AnimalController.java
+++ src/main/java/se/frusunnanbo/servicec/AnimalController.java
@@ -14,35 +14,37 @@ import static java.util.stream.Collectors.toList;
 @RestController
 public class AnimalController {
 
-    private final List<Stuff> stuffs = Arrays.asList(
-            new Stuff("Hufflepuff", "cat", 9),
-            new Stuff("Taco", "dog", 8),
-            new Stuff("Spiky", "hedgehog", 7),
-            new Stuff("Joy", "hedgehog", 4),
-            new Stuff("Smaug", "dragon", 93),
-            new Stuff("Betty", "beaver", 23),
-            new Stuff("Stripey McBitealot", "tiger", 17),
-            new Stuff("Tigger Longwhiskers", "tiger", 10),
-            new Stuff("Simba", "lion", 4)
+    private final List<Animal> animals = Arrays.asList(
+            new Animal("Hufflepuff", "cat", 9, "Only awake at night"),
+            new Animal("Taco", "dog", 8, "Loves bathing"),
+            new Animal("Spiky", "hedgehog", 7, "Absolutely hates milk"),
+            new Animal("Joy", "hedgehog", 4, "Eats snails"),
+            new Animal("Smaug", "dragon", 93, "Loves to be schratched on his belly"),
+            new Animal("Betty", "beaver", 23, "Hides for strangers"),
+            new Animal("Stripey McBitealot", "tiger", 17, "Probably the world's only vegetarian tiger"),
+            new Animal("Tigger Longwhiskers", "tiger", 10, "Born and bred in New York"),
+            new Animal("Simba", "lion", 4, "Loves biting little girls")
     );
 
     @RequestMapping("/animals")
-    public Collection<Stuff> animals(@RequestParam Optional<String> filter) {
+    public Collection<Animal> animals(@RequestParam Optional<String> filter) {
         System.out.println("Got request for /animals");
-        return stuffs.stream()
-                .filter(stuff -> stuff.kind.contains(filter.orElse("")))
+        return animals.stream()
+                .filter(animal -> animal.kind.contains(filter.orElse("")))
                 .collect(toList());
     }
 
-    private static class Stuff {
+    private static class Animal {
         public final String name;
         public final String kind;
         public final int age;
+        public final String funFact;
 
-        private Stuff(String name, String kind, int age) {
+        private Animal(String name, String kind, int age, String funFact) {
             this.name = name;
             this.kind = kind;
             this.age = age;
+            this.funFact = funFact;
         }
     }
 
