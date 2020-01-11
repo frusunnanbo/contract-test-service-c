9c81c8b1cd7b042be36bfbd4bb20f3d144a9d3bd
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-11 19:08:28 +0100

    Adds food schedules for all animals

--- src/main/java/se/frusunnanbo/servicec/AnimalController.java
+++ src/main/java/se/frusunnanbo/servicec/AnimalController.java
@@ -15,15 +15,15 @@ import static java.util.stream.Collectors.toList;
 public class AnimalController {
 
     private final List<Animal> animals = Arrays.asList(
-            new Animal("Hufflepuff", "cat", 9, "Only awake at night"),
-            new Animal("Taco", "dog", 8, "Loves bathing"),
-            new Animal("Spiky", "hedgehog", 7, "Absolutely hates milk"),
-            new Animal("Joy", "hedgehog", 4, "Eats snails"),
-            new Animal("Smaug", "dragon", 93, "Loves to be schratched on his belly"),
-            new Animal("Betty", "beaver", 23, "Hides for strangers"),
-            new Animal("Stripey McBitealot", "tiger", 17, "Probably the world's only vegetarian tiger"),
-            new Animal("Tigger Longwhiskers", "tiger", 10, "Born and bred in New York"),
-            new Animal("Simba", "lion", 4, "Loves biting little girls")
+            new Animal("Hufflepuff", "cat", 9, "Only awake at night", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Taco", "dog", 8, "Loves bathing", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Spiky", "hedgehog", 7, "Absolutely hates milk", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Joy", "hedgehog", 4, "Eats snails", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Smaug", "dragon", 93, "Loves to be schratched on his belly", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Betty", "beaver", 23, "Hides for strangers", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Stripey McBitealot", "tiger", 17, "Probably the world's only vegetarian tiger", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Tigger Longwhiskers", "tiger", 10, "Born and bred in New York", new FoodSchedule("Nothing", "Nothing", "Steak")),
+            new Animal("Simba", "lion", 4, "Loves biting little girls", new FoodSchedule("Nothing", "Nothing", "Steak"))
     );
 
     @RequestMapping("/animals")
@@ -39,13 +39,26 @@ public class AnimalController {
         public final String kind;
         public final int age;
         public final String funFact;
+        public final FoodSchedule foodSchedule;
 
-        private Animal(String name, String kind, int age, String funFact) {
+        private Animal(String name, String kind, int age, String funFact, FoodSchedule foodSchedule) {
             this.name = name;
             this.kind = kind;
             this.age = age;
             this.funFact = funFact;
+            this.foodSchedule = foodSchedule;
         }
     }
 
+    private static class FoodSchedule {
+        public final String morning;
+        public final String lunch;
+        public final String evening;
+
+        private FoodSchedule(String morning, String lunch, String evening) {
+            this.morning = morning;
+            this.lunch = lunch;
+            this.evening = evening;
+        }
+    }
 }
