8dfdb4687b55fdfad2882fd5b3a8cdddcf51302d
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-25 09:00:26 +0100

    Using the new repository

--- src/main/java/se/frusunnanbo/servicec/AnimalController.java
+++ src/main/java/se/frusunnanbo/servicec/AnimalController.java
@@ -6,34 +6,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
+import static se.frusunnanbo.servicec.Animals.BETTY;
+import static se.frusunnanbo.servicec.Animals.HUFFLEPUFF;
+import static se.frusunnanbo.servicec.Animals.JOY;
+import static se.frusunnanbo.servicec.Animals.SIMBA;
+import static se.frusunnanbo.servicec.Animals.SMAUG;
+import static se.frusunnanbo.servicec.Animals.SPIKY;
+import static se.frusunnanbo.servicec.Animals.STRIPEY;
+import static se.frusunnanbo.servicec.Animals.TACO;
+import static se.frusunnanbo.servicec.Animals.TIGGER;
 
 @RestController
 public class AnimalController {
 
     Logger logger = LoggerFactory.getLogger(AnimalController.class);
+    private final AnimalRepository repository;
 
-    private final List<Animal> animals = Arrays.asList(
-            Animals.HUFFLEPUFF,
-            Animals.TACO,
-            Animals.SPIKY,
-            Animals.JOY,
-            Animals.SMAUG,
-            Animals.BETTY,
-            Animals.STRIPEY,
-            Animals.TIGGER,
-            Animals.SIMBA
-    );
+    public AnimalController(AnimalRepository repository) {
+        this.repository = repository;
+        this.repository.setAnimals(
+                HUFFLEPUFF,
+                TACO,
+                SPIKY,
+                JOY,
+                SMAUG,
+                BETTY,
+                STRIPEY,
+                TIGGER,
+                SIMBA);
+    }
 
     @RequestMapping("/animals")
     public Collection<Animal> animals(@RequestParam Optional<String> filter) {
         logger.info("Got request for /animals");
-        return animals.stream()
+        return repository.getAll().stream()
                 .filter(animal -> animal.kind.contains(filter.orElse("")))
                 .collect(toList());
     }
