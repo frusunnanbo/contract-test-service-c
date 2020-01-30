7069bebc3400ab0f6931745e1d7080da773cc7d2
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-01-30 13:25:33 +0100

    Adds pacts and state change for service A

--- src/main/java/se/frusunnanbo/servicec/PactStateController.java
+++ src/main/java/se/frusunnanbo/servicec/PactStateController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static se.frusunnanbo.servicec.Animals.HUFFLEPUFF;
+import static se.frusunnanbo.servicec.Animals.JOY;
 import static se.frusunnanbo.servicec.Animals.SMAUG;
+import static se.frusunnanbo.servicec.Animals.SPIKY;
 import static se.frusunnanbo.servicec.Animals.STRIPEY;
 
 @RestController
@@ -28,6 +30,9 @@ public class PactStateController {
         if ("there are >= 3 animals".equals(state.state)) {
             logger.info("3 animals");
             animalRepository.setAnimals(STRIPEY, SMAUG, HUFFLEPUFF);
+        } else if ("there are 2 hedgehogs and 1 cat".equals(state.state)) {
+            logger.info("3 animals");
+            animalRepository.setAnimals(SPIKY, HUFFLEPUFF, JOY);
         }
 
         return "OK";
